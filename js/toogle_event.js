22f6de0a700999f4ceed49b5cb663a9d909c9082
Brandon Alexis Machado Ambuila <brandonmachado159@gmail.com>
2021-02-19 18:49:29 -0500

    se implemento navegacion responsive

---
+++ js/toogle_event.js
@@ -0,0 +1,7 @@
+
+function Toogle(){
+    const toogle = document.querySelector('.nav__toogle');
+    const barra = document.querySelector('.nav__links ul');
+    toogle.classList.toggle('active');
+    barra.classList.toggle('active');
+}
