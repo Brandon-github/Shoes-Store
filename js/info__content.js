399fdd6123f280e49759340e138ffd44c17460b3
Brandon Alexis Machado Ambuila <brandonmachado159@gmail.com>
2021-02-22 18:51:48 -0500

    se agrego estructura de botones creados

---
+++ js/info__content.js
@@ -0,0 +1,11 @@
+const imagen = document.querySelector('.content__img');
+for (let i = 0; i < 21; i++) {
+    var obj = document.createElement('div');
+    obj.classList.add('img__obj');
+    imagen.appendChild(obj);
+}
+
+
+
+
+
