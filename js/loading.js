849787281f45c8aa073c82ce3b46f0b030beb55a
Brandon Alexis Machado Ambuila <brandonmachado159@gmail.com>
2021-02-26 16:46:29 -0500

    se agrego opacidad de pantalla de carga

---
+++ js/loading.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    var loading = document.querySelector('#loading');
+
+    loading.style.transition = "0.3s linear"
+    loading.style.opacity = "0";
+    loading.style.visibility = "hidden";
+}
